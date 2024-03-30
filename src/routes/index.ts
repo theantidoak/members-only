ee93b5cc3830136852908a23ab581890d57c5b8d
theantidoak <tcdoak@gmail.com>
2024-03-30 21:47:03 +0100

    init commit

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+export const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
+  res.render('index', { title: 'Express' });
+});
