8a825faea6c9a6ed6c3aa521ee1da95cee9fb7f4
theantidoak <tcdoak@gmail.com>
2024-04-04 00:44:29 +0200

    add sign up form

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,10 @@ import express, { Request, Response, NextFunction } from 'express';
 export const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req: Request, res: Response, next: NextFunction) {
+router.get('/', function(_req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
+});
+
+router.get('/sign-up', function(_req: Request, res: Response, next: NextFunction) {
+  res.render('sign-up-form', { title: 'Sign Up' });
 });
