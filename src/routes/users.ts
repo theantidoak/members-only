72ae7e7cc19d0d507999e560be68537518746b1c
theantidoak <tcdoak@gmail.com>
2024-04-04 22:19:03 +0200

    change sign-up to register

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcryptjs';
 import passport from 'passport';
+import { User } from '../models/user';
 
 export const router = express.Router();
 
@@ -8,8 +10,8 @@ router.get('/', function(_req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
 });
 
-router.get('/sign-up', function(_req: Request, res: Response, next: NextFunction) {
-  res.render('sign-up-form', { title: 'Sign Up' });
+router.get('/register', function(_req: Request, res: Response, next: NextFunction) {
+  res.render('register-form', { title: 'Sign Up' });
 });
 
 router.get('/login', function(_req: Request, res: Response, next: NextFunction) {
@@ -17,6 +19,25 @@ router.get('/login', function(_req: Request, res: Response, next: NextFunction) 
 });
 
 /* POST */
+
+router.post("/register", async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    } else {
+      try {
+        const user = new User({
+          username: req.body.username,
+          password: hashedPassword
+        });
+        const result = await user.save();
+        res.redirect("/");
+      } catch(err) {
+        return next(err);
+      };
+    }
+  });
+});
 
 router.post(
   '/login', 
