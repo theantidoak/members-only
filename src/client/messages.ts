76a68365239ab8f1cbb4a1f5026a9d84b1c55225
theantidoak <tcdoak@gmail.com>
2024-04-06 19:05:08 +0200

    add edit message logic

--- src/client/messages.ts
+++ src/client/messages.ts
@@ -2,13 +2,78 @@ import { convertToDomNode } from '../utils/shortcodes';
 
 document.addEventListener('DOMContentLoaded', () => {
   const createMessageBtn = document.querySelector('.messages__create-btn');
-  createMessageBtn?.addEventListener('click', async () => {
-    try {
-      const sectionFetch = await fetch('/messages/create');
-      const section = await sectionFetch.text();
-      console.log(convertToDomNode(section));
-    } catch(error) {
-      console.error('Error loading create message form: ', error);
-    }
+  createMessageBtn?.addEventListener('click', handleCreateBtn);
+
+  const editMessageBts = document.querySelectorAll('.messages__edit-btn');
+  [...editMessageBts].forEach((editBtn) => {
+    editBtn.addEventListener('click', handleEditBtn);
   });
 });
+
+async function handleCreateBtn() {
+  try {
+    const main = document.querySelector('main') as HTMLElement;
+    const formFetch = await fetch('/messages/create');
+    const form = await formFetch.text();
+    const formNode = convertToDomNode(form);
+    const section = document.createElement('section');
+    section.appendChild(formNode);
+    main.appendChild(section);
+
+    const cancelBtn = formNode.querySelector('.message__cancel-button');
+    cancelBtn?.addEventListener('click', handleCancelCreateBtn);
+  } catch(error) {
+    console.error('Error loading create message form: ', error);
+  }
+}
+
+async function handleEditBtn(e: Event) {
+  const editBtn = e.currentTarget as HTMLButtonElement;
+
+  try {
+    const card = editBtn.parentElement as HTMLLIElement;
+    const messageId = card.dataset.id as string;
+    const formFetch = await fetch(`/messages/update?id=${messageId}`);
+    const form = await formFetch.text();
+    const formNode = convertToDomNode(form);
+    const listItem = document.createElement('li');
+    listItem.setAttribute('data-id', messageId);
+    listItem.classList.add('messages__item');
+    listItem.appendChild(formNode);
+    editBtn.removeEventListener('click', handleEditBtn);
+    card?.replaceWith(listItem);
+
+    const cancelBtn = formNode.querySelector('.message__cancel-button');
+    cancelBtn?.addEventListener('click', handleCancelUpdateBtn);
+  } catch(error) {
+    console.error('Error loading update message form: ', error);
+  }
+}
+
+async function handleCancelUpdateBtn(e: Event) {
+  const cancelBtn = e.currentTarget as HTMLButtonElement;
+  const formListItem = cancelBtn.parentElement?.parentElement?.parentElement as HTMLLIElement;
+
+  try {
+    const listItemFetch = await fetch(`/messages/card?id=${formListItem.dataset.id}`);
+    const listItem = await listItemFetch.text();
+    const listItemNode = convertToDomNode(listItem);
+    cancelBtn.removeEventListener('click', handleCancelUpdateBtn);
+    formListItem.replaceWith(listItemNode);
+
+    const editMessageBts = document.querySelectorAll('.messages__edit-btn');
+    [...editMessageBts].forEach((btn) => {
+      btn.addEventListener('click', handleEditBtn);
+    });
+  } catch(error) {
+    console.error('Error loading update message form: ', error);
+  }
+}
+
+async function handleCancelCreateBtn(e: Event) {
+  const cancelBtn = e.currentTarget as HTMLButtonElement;
+  cancelBtn.removeEventListener('click', handleCancelCreateBtn);
+  const section = cancelBtn.parentElement?.parentElement?.parentElement as HTMLLIElement;
+
+  section.remove();
+}
--- src/routes/messages.ts
+++ src/routes/messages.ts
@@ -12,26 +12,39 @@ export const router = express.Router();
 router.get('/', async function(_req: Request, res: Response, next: NextFunction) {
   const user = res.locals.user;
   const isLoggedIn = user.first_name.length > 0 ? true : false;
-  const messages = await Message.find({ user : user.id }).exec();
+  const messages = await Message.find({ user : user.id }).sort({ time_stamp: -1 }).exec();
 
   res.render('messages', { title: 'Messages', messages: messages, errors: undefined, isLoggedIn: isLoggedIn });
+});
+
+router.get('/card', async function(req: Request, res: Response, next: NextFunction) {
+  const id = req.query.id ?? '';
+  const message = await Message.findOne({ '_id' : id }).exec();
+
+  res.render('message-card', { title: 'Create message', message: message });
 });
 
 router.get('/create', function(_req: Request, res: Response, next: NextFunction) {
   const user = res.locals.user;
   const isLoggedIn = user.first_name.length > 0 ? true : false;
-  res.render('message-form', { title: 'Create message', message: undefined, errors: undefined, isLoggedIn: isLoggedIn });
+  
+  res.render('message-form', { title: 'Create message', message: undefined, errors: undefined, isLoggedIn: isLoggedIn, form: 'create' });
 });
 
-router.get('/update', function(_req: Request, res: Response, next: NextFunction) {
+router.get('/update', async function(req: Request, res: Response, next: NextFunction) {
   const user = res.locals.user;
-  const message = Message.find({ 'user.id' : user.id }).exec();
+  const id = req.query.id ?? '';
+  const message = await Message.findOne({ '_id' : id }).exec();
   const isLoggedIn = user.first_name.length > 0 ? true : false;
-  res.render('message-form', { title: 'Update message', message: message, errors: undefined, isLoggedIn: isLoggedIn });
+
+  res.render('message-form', { title: 'Update message', message: message, errors: undefined, isLoggedIn: isLoggedIn, form: 'update' });
 });
 
 /* POST */
 router.post('/create', [
+  body('id')
+    .trim()
+    .escape(),
   body('title')
     .trim()
     .isLength({ min: 1 })
@@ -48,12 +61,13 @@ router.post('/create', [
 
     if (!error.isEmpty() || user.id === null) {
       const errors = error.array();
+      const isLoggedIn = user.first_name.length > 0 ? true : false;
 
       if (user.id === null) {
         errors.push({
           type: 'field',
           value: req.body.text,
-          msg: 'Faild to submit. You must get a member to submit a message.',
+          msg: 'Failed to submit. You must be a member to submit a message.',
           path: 'text',
           location: 'body'
         });
@@ -62,7 +76,9 @@ router.post('/create', [
       res.render("message-form", { 
         title: 'Create message', 
         message: req.body,
-        errors: errors
+        errors: errors,
+        isLoggedIn: isLoggedIn,
+        form: 'create'
       });
 
       return;
@@ -75,6 +91,66 @@ router.post('/create', [
         user: user.id
       });
       const result = await message.save();
+
+      res.redirect("/messages");
+    } catch (err) {
+      return next(err);
+    }
+
+  })
+]);
+
+router.post('/update', [
+  body('id')
+    .trim()
+    .escape(),
+  body('title')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Title is required.')
+    .escape(),
+  body('text')
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage('Message is required.')
+    .escape(),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    const user = res.locals.user;
+
+    if (!error.isEmpty() || user.id === null) {
+      const errors = error.array();
+      const isLoggedIn = user.first_name.length > 0 ? true : false;
+
+      if (user.id === null) {
+        errors.push({
+          type: 'field',
+          value: req.body.text,
+          msg: 'Failed to submit. You must be a member to update a message.',
+          path: 'text',
+          location: 'body'
+        });
+      }
+
+      res.render("message-form", { 
+        title: 'Create message', 
+        message: req.body,
+        errors: errors,
+        isLoggedIn: isLoggedIn,
+        form: 'update'
+      });
+
+      return;
+    }
+
+    try {
+      const newMessage = {
+        title: req.body.title,
+        text: req.body.text,
+        time_stamp: Date.now(),
+        user: user.id
+      };
+      const updatedApplication = await Message.findByIdAndUpdate(req.body.id, newMessage, { new: true });
 
       res.redirect("/messages");
     } catch (err) {
