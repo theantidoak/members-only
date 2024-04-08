fce1b7bd0fe0289bd5414aee335599e50fccbef2
theantidoak <tcdoak@gmail.com>
2024-04-09 01:02:30 +0200

    start styling

--- src/client/messages.ts
+++ src/client/messages.ts
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     editBtn.addEventListener('click', handleEditBtn);
   });
 
-  const deleteMessageBtns = document.querySelectorAll('.message__delete-btn');
+  const deleteMessageBtns = document.querySelectorAll('.messages__delete-btn');
   [...deleteMessageBtns].forEach((deleteBtn) => {
     (deleteBtn as any).prop = { btn: deleteBtn, error: undefined }
     deleteBtn.addEventListener('click', handleDeleteBtn);
@@ -34,7 +34,14 @@ function getFetchHeaders() {
 async function handleCancelCreateBtn(e: Event) {
   const cancelBtn = e.currentTarget as HTMLButtonElement;
   cancelBtn.removeEventListener('click', handleCancelCreateBtn);
-  const section = cancelBtn.parentElement?.parentElement?.parentElement as HTMLLIElement;
+  const section = cancelBtn.parentElement?.parentElement?.parentElement?.parentElement as HTMLLIElement;
+  const main = document.querySelector('main') as HTMLElement;
+
+  [...main.children].forEach((child, i, arr) => {
+    if (arr.indexOf(child) !== arr.length - 1) {
+      child.removeAttribute('style');
+    }
+  });
 
   section.remove();
 }
@@ -49,9 +56,15 @@ async function handleCreateBtn() {
     section.appendChild(formNode);
     main.appendChild(section);
 
+    [...main.children].forEach((child, i, arr) => {
+      if (arr.indexOf(child) !== arr.length - 1) {
+        child.setAttribute('style', 'filter: blur(6px); pointer-events: none');
+      }
+    });
+
     const textarea = formNode.querySelector("textarea") as HTMLTextAreaElement;
     textarea.addEventListener('input', handleTextArea);
-    const cancelBtn = formNode.querySelector('.message__cancel-button');
+    const cancelBtn = formNode.querySelector('.messages__cancel-btn');
     cancelBtn?.addEventListener('click', handleCancelCreateBtn);
   } catch(error) {
     console.error('Error loading create message form: ', error);
@@ -62,7 +75,7 @@ async function handleEditBtn(e: Event) {
   const editBtn = e.currentTarget as HTMLButtonElement;
 
   try {
-    const card = editBtn.parentElement as HTMLLIElement;
+    const card = editBtn.parentElement?.parentElement as HTMLLIElement;
     const messageId = card.id as string;
     const formFetch = await fetch(`/messages/update?id=${messageId}`, getFetchHeaders());
     const form = await formFetch.text();
@@ -74,7 +87,7 @@ async function handleEditBtn(e: Event) {
     editBtn.removeEventListener('click', handleEditBtn);
     card.replaceWith(listItem);
 
-    const cancelBtn = formNode.querySelector('.message__cancel-button');
+    const cancelBtn = formNode.querySelector('.messages__cancel-btn');
     cancelBtn?.addEventListener('click', handleCancelEditBtn);
   } catch(error) {
     console.error('Error loading update message form: ', error);
@@ -83,7 +96,7 @@ async function handleEditBtn(e: Event) {
 
 async function handleCancelEditBtn(e: Event) {
   const cancelBtn = e.currentTarget as HTMLButtonElement;
-  const formListItem = cancelBtn.parentElement?.parentElement?.parentElement as HTMLLIElement;
+  const formListItem = cancelBtn.parentElement?.parentElement?.parentElement?.parentElement as HTMLLIElement;
 
   try {
     const listItemFetch = await fetch(`/messages/message?id=${formListItem.dataset.id}`, getFetchHeaders());
@@ -92,7 +105,7 @@ async function handleCancelEditBtn(e: Event) {
     cancelBtn.removeEventListener('click', handleCancelEditBtn);
     formListItem.replaceWith(listItemNode);
 
-    const deleteMessageBtn = document.querySelector('.message__delete-btn');
+    const deleteMessageBtn = document.querySelector('.messages__delete-btn');
     (deleteMessageBtn as any).prop = { btn: deleteMessageBtn, error: undefined };
     deleteMessageBtn?.addEventListener('click', handleDeleteBtn);
     const editMessageBtn = listItemNode.querySelector('.messages__edit-btn');
@@ -128,7 +141,7 @@ async function handleDeleteMessageBtn(e: Event) {
 
     const result = await deleteItemFetch.json();
     const listItem = document.querySelector(`li#${messageId}`) as HTMLLIElement;
-    const deleteMessageBtn = listItem.querySelector('.message__delete-btn') as HTMLButtonElement;
+    const deleteMessageBtn = listItem.querySelector('.messages__delete-btn') as HTMLButtonElement;
     const prop = { btn: deleteMessageBtn, error: result.message };
 
     if (result.success) {
@@ -150,7 +163,7 @@ async function handleDeleteBtn(this: { prop: { btn: HTMLButtonElement, error: st
 
   try {
     const main = document.querySelector('main') as HTMLElement;
-    const card = deleteBtn.parentElement as HTMLLIElement;
+    const card = deleteBtn.parentElement?.parentElement as HTMLLIElement;
     const messageId = card.id as string;
     const deleteItemFetch = await fetch(`/messages/delete?error=${error}`, getFetchHeaders());
     const deleteItem = await deleteItemFetch.text();
